cb0bfdca5ac7f03e6460c9ecd305f47dd3da8d0a
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-09-13 19:13:49 -0300

    Updates

---
+++ precache-manifest.5f3c28834b62fc7d40648b92313dfaec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294c401c17d08403507edd77aa3f0621",
+    "url": "/covid19-react/index.html"
+  },
+  {
+    "revision": "ef0122c129bd3dc8ac8d",
+    "url": "/covid19-react/static/css/main.1c32bb19.chunk.css"
+  },
+  {
+    "revision": "f8f5c819073248d4081c",
+    "url": "/covid19-react/static/js/2.8eb78b2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid19-react/static/js/2.8eb78b2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef0122c129bd3dc8ac8d",
+    "url": "/covid19-react/static/js/main.7c01f1fb.chunk.js"
+  },
+  {
+    "revision": "089a1a07f8d572efe7ab",
+    "url": "/covid19-react/static/js/runtime-main.ee776a7a.js"
+  }
+]);
